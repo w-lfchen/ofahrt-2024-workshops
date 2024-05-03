4fc9d06a4ddd13a84119fa821670c6dd2bbad795
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2024-05-03 23:00:42 +0200

    feat: use rose-pine-moon theme

--- flake.nix
+++ flake.nix
@@ -1,14 +1,20 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rose-pine-moon = {
+      url = "https://raw.githubusercontent.com/rnd195/marp-community-themes/live/themes/rose-pine-moon.css";
+      flake = false;
+    };
+  };
 
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs, rose-pine-moon }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      packages.${system}.default = (pkgs.writeShellScriptBin "marp-build" "${pkgs.marp-cli}/bin/marp $1");
+      packages.${system}.default = (pkgs.writeShellScriptBin "marp-build" "${pkgs.marp-cli}/bin/marp --theme ${rose-pine-moon} $1");
 
       devShells.${system}.default = pkgs.mkShell { packages = with pkgs; [ marp-cli ]; };
 
